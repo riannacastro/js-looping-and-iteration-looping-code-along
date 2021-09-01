5137afdf7f945cd61f81948ad9966b9521e89ecc
Rianna <riannaxcastro@gmail.com>
2021-08-31 17:12:38 -1000

    commit tree 4c0b64e865553dc9f1022ce7a6e34e4e38592e86

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+
+function writeCards(arrayNames, eventName) {
+    let newCard = []
+    for (let i = 0; i < arrayNames.length; i++) {
+        newCard[i] = `Thank you, ${arrayNames[i]}, for the wonderful ${eventName} gift!`
+    }
+    return newCard
+}
+
+
+function countDown(num) {
+    while (num >= 0) {
+        console.log(num --)
+    }
+}
